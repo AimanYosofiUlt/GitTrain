2c4842d19974e81e1e2d797068e41c34b0baf559
Ultimate <aimanyosofiult>
2022-04-16 13:55:07 +0200

    Add AddressApi Injection

--- app/src/main/java/com/example/gittrain/app/di/Module.java
+++ app/src/main/java/com/example/gittrain/app/di/Module.java
@@ -1,5 +1,6 @@
 package com.example.gittrain.app.di;
 
+import android.app.Application;
 import android.content.Context;
 
 import com.example.gittrain.dataacess.local.daos.AddressDao;
@@ -7,15 +8,25 @@ import com.example.gittrain.dataacess.local.daos.NoteDao;
 import com.example.gittrain.dataacess.local.daos.UserDao;
 import com.example.gittrain.dataacess.local.db.AppDatabase;
 import com.example.gittrain.dataacess.local.entites.Address;
+import com.example.gittrain.dataacess.remote.api.AddressApi;
 
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.android.components.ActivityComponent;
 import dagger.hilt.android.scopes.ActivityScoped;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 @dagger.Module
 @InstallIn(ActivityComponent.class)
 public class Module {
+
+    @ActivityScoped
+    @Provides
+    Context provideContext(Application application) {
+        return application.getApplicationContext();
+    }
+
     @ActivityScoped
     @Provides
     AppDatabase provideAppDatabase(Context context) {
@@ -38,5 +49,14 @@ public class Module {
     @Provides
     AddressDao provideAddressDao(AppDatabase appDatabase) {
         return appDatabase.addressDao();
+    }
+
+    @ActivityScoped
+    @Provides
+    AddressApi provideAddressApi() {
+        return new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build().create(AddressApi.class);
     }
 }
--- app/src/main/java/com/example/gittrain/presntation/ui/main/MainActivityViewModel.java
+++ app/src/main/java/com/example/gittrain/presntation/ui/main/MainActivityViewModel.java
@@ -1,16 +1,23 @@
 package com.example.gittrain.presntation.ui.main;
 
 import android.app.Application;
+
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+
 import com.example.gittrain.dataacess.local.repos.NoteRepo;
 import com.example.gittrain.dataacess.local.repos.UserRepo;
+import com.example.gittrain.dataacess.remote.api.AddressApi;
+
 import javax.inject.Inject;
+
 public class MainActivityViewModel extends AndroidViewModel {
     @Inject
     UserRepo userRepo;
     @Inject
     NoteRepo noteRepo;
+    @Inject
+    AddressApi api;
 
     @Inject
     public MainActivityViewModel(@NonNull Application application) {
